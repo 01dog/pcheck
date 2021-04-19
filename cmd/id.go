0391229e1b0f3463ae0cad8b5bf97b374ef8eb89
01dog <bellwitch@pm.me>
2021-04-18 21:56:00 -0400

    wip change license, work on id command, config

--- cmd/id.go
+++ cmd/id.go
@@ -1,51 +1,36 @@
-/*
-Copyright © 2021 NAME HERE 01dog@pm.me
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var key string
 
 // idCmd represents the id command
 var idCmd = &cobra.Command{
 	Use:   "id",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("id called")
+	Short: "returns steamid information on a user",
+	Long: `long msg`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		key = viper.GetString("key")
+		return test(key)
 	},
+}
+
+func test(key string) error {
+	fmt.Println(key)
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(idCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// idCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// idCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	idCmd.Flags().StringVarP(&key, "key", "k", "", "steamid.uk API key (overrides config file, optional)")
+	if err := viper.BindPFlag("key", idCmd.Flags().Lookup("key")); err != nil {
+		log.Fatal("unable to bind flag: ", err)
+	}
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,26 +1,9 @@
-/*
-Copyright © 2021 NAME HERE 01dog@pm.me
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
-	"os"
+	"log"
 
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
@@ -29,16 +12,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pcheck",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "small tool for player ban info",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -54,11 +28,11 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pcheck.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $PWD/config.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -67,19 +41,17 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".pcheck" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".pcheck")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+		viper.AddConfigPath(".")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		if cfgFile != "" {
+			log.Println("config specified but unable to read it, using defaults")
+		}
 	}
 }
